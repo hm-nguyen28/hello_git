3a89fac984e5c1adae5e207e77217682e17dae00
Duc Loc Nguyen <hm-nguyen28@hm.edu>
2021-03-28 13:30:20 +0200

    First work commits

---
+++ src/ProjectToJararchive/Main.java
@@ -0,0 +1,9 @@
+package ProjectToJararchive;
+
+public class Main {
+  public static void main(String[] args) {
+    Student student = new Student(18, "Loc D");
+
+    System.out.println("Hello World: " + student);
+  }
+}
